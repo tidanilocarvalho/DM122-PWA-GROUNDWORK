ccf60589ef373c986d6a4c35770ea04979833289
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-21 12:30:15 -0300

    Add frist test with offline mode

--- DM122-T195-AppShell-main/sw.js
+++ DM122-T195-AppShell-main/sw.js
@@ -17,6 +17,16 @@ self.addEventListener('activate', event => {
   console.log('[Service Worker] Activating service work...')
 })
 
+async function networkFirst(request) {
+  try {
+    return await fetch(request);
+  } catch {
+    const cache = await caches.open('app-shell-v1');
+    return cache.match('offline.html');
+  }
+}
+
 self.addEventListener('fetch', event => {
-  console.log('[Service Worker] Fetch event...', event)
+  //console.log('[Service Worker] Fetch event...', event)
+  event.respondWith(networkFirst(event.request));
 });

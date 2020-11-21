ff03a894759da01893c67a203d9c0f9b0b12a5b6
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-21 15:19:13 -0300

    Add feature to run app shell offline

--- DM122-T195-AppShell-main/sw.js
+++ DM122-T195-AppShell-main/sw.js
@@ -1,15 +1,26 @@
-const cacheName = 'app-shell-v10';
+const cacheName = 'app-shell-v1234';
 const assetsToCache = [
-    'offline.html', 'index.html'
+'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+'https://fonts.googleapis.com/css?family=Roboto:400,700',
+'https://fonts.googleapis.com/icon?family=Material+Icons',
+'assets/images/pwa-logo.png',
+'assets/js/material.min.js',
+//'assets/css/style.css',
+'assets/js/app.js',
+'favicon.ico',
+'index.html',
+'/'
 ];
 
 async function cacheStaticAssets() {
-    try {
-        const cache = await caches.open(cacheName);
-        return cache.addAll(assetsToCache);
-    } catch (error) {
-        console.log("Failed to install assets cache ", error);
-    }
+  try {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(assetsToCache);
+  } catch (error) {
+    console.log("Failed to install assets cache ", error);
+  }
 }
 
 async function networkFirst(request) {
@@ -17,7 +28,7 @@ async function networkFirst(request) {
     return await fetch(request);
   } catch {
     const cache = await caches.open(cacheName);
-    return cache.match('offline.html');
+    return cache.match(request);
   }
 }
 

1e2cb57c9885467fafefd7ab39613433edbd7de3
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-21 12:18:11 -0300

    Add offline.html to cache

--- DM122-T195-AppShell-main/sw.js
+++ DM122-T195-AppShell-main/sw.js
@@ -1,5 +1,15 @@
 self.addEventListener('install', event => {
   console.log('[Service Worker] Installing service worker...');
+
+  event.waitUntil(
+      caches.open('app-shell-v1')
+        .then(cache => {
+            return cache.addAll([
+              "offline.html"
+            ])
+        })
+    );
+
   self.skipWaiting();
 });
 

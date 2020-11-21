2ec81b68928eec36f07f14a6dc0963bfa2d7a645
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-21 12:01:03 -0300

    Use spaces instead tab

--- DM122-T195-AppShell-main/sw.js
+++ DM122-T195-AppShell-main/sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', event => {
-    console.log('[Service Worker] Installing service worker...');
-    self.skipWaiting();
-  });
+  console.log('[Service Worker] Installing service worker...');
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activating service work...')
+  console.log('[Service Worker] Activating service work...')
 })
 
 self.addEventListener('fetch', event => {
-    console.log('[Service Worker] Fetch event...', event)
+  console.log('[Service Worker] Fetch event...', event)
 });

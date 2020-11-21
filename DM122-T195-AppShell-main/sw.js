8e1c9b3e1c4ebe7336756adbb978a2ed09b5130a
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-21 15:10:31 -0300

    Add function to clear old cache

--- DM122-T195-AppShell-main/sw.js
+++ DM122-T195-AppShell-main/sw.js
@@ -1,30 +1,49 @@
-self.addEventListener('install', event => {
-  console.log('[Service Worker] Installing service worker...');
+const cacheName = 'app-shell-v10';
+const assetsToCache = [
+    'offline.html', 'index.html'
+];
 
-  event.waitUntil(
-      caches.open('app-shell-v1')
-        .then(cache => {
-            return cache.addAll([
-              "offline.html"
-            ])
-        })
-    );
-
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('[Service Worker] Activating service work...')
-})
+async function cacheStaticAssets() {
+    try {
+        const cache = await caches.open(cacheName);
+        return cache.addAll(assetsToCache);
+    } catch (error) {
+        console.log("Failed to install assets cache ", error);
+    }
+}
 
 async function networkFirst(request) {
   try {
     return await fetch(request);
   } catch {
-    const cache = await caches.open('app-shell-v1');
+    const cache = await caches.open(cacheName);
     return cache.match('offline.html');
   }
 }
+
+function removeOldCache(cacheKey) {
+  if (cacheKey !== cacheName) {
+    console.log('[Service Worker] removing old cache');
+    return caches.delete(cacheKey);
+  }
+}
+
+async function cacheCleanUp() {
+    const keyList = await caches.keys();
+    return Promise.all(keyList.map(removeOldCache));
+}
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing service worker...');
+  event.waitUntil(cacheStaticAssets());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating service work...')
+  event.waitUntil(cacheCleanUp());
+  self.clients.claim();
+})
 
 self.addEventListener('fetch', event => {
   //console.log('[Service Worker] Fetch event...', event)

0c8dd86d2579238bf0ece3db7154fa0b5da6b773
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-28 13:54:44 -0300

    Refactor app.js

--- DM122-T195-Todo-main/assets/js/app.js
+++ DM122-T195-Todo-main/assets/js/app.js
@@ -1,11 +1,19 @@
-'use strict'
+class App {
+  constructor() {
+    this.registerServiceWorker();
+  }
 
-if ('serviceWorker' in navigator) {
-  const onsuccess = () => console.log('[Service Worker] Registered');
-  const onfailure = () => console.log('[Service Worker] Failed');
-
-  navigator.serviceWorker
-    .register('sw.js')
-    .then(onsuccess)
-    .catch(onfailure);
+  registerServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      const onsuccess = () => console.log('[Service Worker] Registered');
+      const onfailure = () => console.log('[Service Worker] Failed');
+    
+      navigator.serviceWorker
+        .register('sw.js')
+        .then(onsuccess)
+        .catch(onfailure);
+    }
+  }
 }
+
+new App();

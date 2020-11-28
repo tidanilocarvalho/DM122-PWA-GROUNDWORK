ba2fc1d105b9b2c77db2e84732653225487b7c95
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-28 15:37:41 -0300

    Add todos to the db

--- DM122-T195-Todo-main/assets/js/HtmlService.js
+++ DM122-T195-Todo-main/assets/js/HtmlService.js
@@ -1,18 +1,48 @@
 export default class HtmlService {
-    #todoService;
-    
     constructor(todoService) {
-        this.#todoService = todoService;
-        this.bindFormEvent();
+      this.todoService = todoService;
+      this.bindFormEvent();
+      this.listTasks();
     }
-    
+  
     bindFormEvent() {
-        const form = document.querySelector("form");
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            console.log("submited " + form.item.value);
-            form.reset();
-            form.item.focus();
-        });
+      const form = document.querySelector("form");
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        this.addTask(form.item.value);
+        form.reset();
+        form.item.focus();
+      });
     }
-}
+  
+    async addTask(description) {
+      const task = { description, done: false };
+      const taskId = await this.todoService.save(task);
+      task.id = taskId;
+      this.addToHtmlList(task);
+    }
+  
+    async listTasks() {
+      const tasks = await this.todoService.getAll();
+      tasks.forEach((task) => this.addToHtmlList(task));
+    }
+  
+    addToHtmlList(task) {
+      const ul = document.querySelector("ul");
+      const li = document.createElement("li");
+      const span = document.createElement("span");
+      const button = document.createElement("button");
+  
+      li.setAttribute("data-item-id", task.id);
+      span.textContent = task.description;
+      button.textContent = "x";
+  
+      if (task.done) {
+        li.classList.add("done");
+      }
+  
+      li.appendChild(span);
+      li.appendChild(button);
+      ul.appendChild(li);
+    }
+  }
--- DM122-T195-Todo-main/assets/js/TodoService.js
+++ DM122-T195-Todo-main/assets/js/TodoService.js
@@ -1,41 +1,44 @@
-import Dexie from 'https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs';
+import Dexie from "https://cdn.jsdelivr.net/npm/dexie@3.0.3/dist/dexie.mjs";
 
 let db;
 
 export default class TodoService {
-    constructor() {
-        this.initializeDB();
-    }
+  constructor() {
+    this.initializeDB();
+  }
 
-    initializeDB() {
-        db = new Dexie('todoDB');
+  initializeDB() {
+    db = new Dexie("todoDB");
 
-        db.version(1).stores({
-            tasks: '++id, description'
-        })
+    db.version(1).stores({
+      tasks: "++id,description",
+    });
 
-        db.on("populate", async () => {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-            const jsonData = await response.json();
-            await db.tasks.bulkPut(jsonData);
-        });
+    db.on("populate", async () => {
+      await db.tasks.bulkPut([
+        { description: "Learn JavaScript", done: true },
+        { description: "Learn TypeScript", done: false },
+        { description: "Learn PWA", done: false },
+        { description: "Learn HTML5 APIs", done: false },
+      ]);
+    });
 
-        db.open();
-    }
+    db.open();
+  }
 
-    getAll() {
-        return db.tasks.toArray();
-    }
+  getAll() {
+    return db.tasks.toArray();
+  }
 
-    get(id) {
-        return db.tasks.get(id);
-    }
+  get(id) {
+    return db.tasks.get(id);
+  }
 
-    save(task) {
-        return db.tasks.put(task);
-    }
+  save(task) {
+    return db.tasks.put(task);
+  }
 
-    delete(id) {
-        return db.tasks.delete(id);
-    }
+  delete(id) {
+    return db.tasks.delete(id);
+  }
 }

9ae432270463b08b11c5d9ae255762b8d0a4c73a
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-28 10:59:50 -0300

    Implement Save To Storage

--- WEB-STORAGE/app.js
+++ WEB-STORAGE/app.js
@@ -1,5 +1,24 @@
 class App {
     constructor() {
         console.log("Initialized");
+        this.bindButtonListener();
+    }
+
+    bindButtonListener() {
+        const button = document.querySelector("button");
+        button.addEventListener("click", () => {
+            this.saveToStorage();
+        })
+    }
+
+    saveToStorage() {
+        const keyInput = document.getElementById("key");
+        const valueInput = document.getElementById("value");
+
+        if (keyInput.value && valueInput.value) {
+            localStorage.setItem(keyInput.value, valueInput.value);
+        }
     }
 }
+
+new App();

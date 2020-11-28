37ffe49184d243182286ae2e12be297eb77160a4
Danilo Carvalho <danilo.carvalho@inatel.br>
2020-11-28 15:07:11 -0300

    Add TodoService.js methods to handle Dexie.js

--- DM122-T195-Todo-main/assets/js/TodoService.js
+++ DM122-T195-Todo-main/assets/js/TodoService.js
@@ -10,21 +10,32 @@ export default class TodoService {
     initializeDB() {
         db = new Dexie('todoDB');
 
-        db.version("1").stores({
+        db.version(1).stores({
             tasks: '++id, description'
         })
 
         db.on("populate", async () => {
-            await db.tasks.bulkPut([
-                { description: "Learn JavaScript", done: true},
-                { description: "Learn TypeScript", done: false},
-                { description: "Learn PWA", done: false},
-                { description: "Learn HTML5 APIs", done: false},
-            ])
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const jsonData = await response.json();
+            await db.tasks.bulkPut(jsonData);
         });
 
         db.open();
     }
 
+    getAll() {
+        return db.tasks.toArray();
+    }
 
+    get(id) {
+        return db.tasks.get(id);
+    }
+
+    save(task) {
+        return db.tasks.put(task);
+    }
+
+    delete(id) {
+        return db.tasks.delete(id);
+    }
 }
